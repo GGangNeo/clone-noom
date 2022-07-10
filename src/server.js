065bdc81513eaa9977dee44776f0c5fbc13f4917
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-10 15:24:40 +0900

    frontend setup
    view engine, public folder, home render

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import express from 'express';
 
 const app = express();
-
-console.log('hi');
-
-app.listen(3000);
+// set view engine as pus
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+// set public folder, expose for user
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
+// catch all url
+app.get('/*', (req, res) => res.redirect('/'));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
