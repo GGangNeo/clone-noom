69c7b41dec43cb47e53c79e2fc2fd4092c656d19
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-12 09:20:13 +0900

    websocket setup
    install ws

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
-import express from 'express';
+import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
+
 // set view engine as pus
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 // set public folder, expose for user
-app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
 // catch all url
-app.get('/*', (req, res) => res.redirect('/'));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+wss.on("connection", (socketServer) => {
+  console.log(socketServer);
+});
+
+server.listen(8080, handleListen);
