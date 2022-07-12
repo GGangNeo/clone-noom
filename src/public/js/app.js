c448ffd9c63d44c50e5c436afb4b1bcbb7559179
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-12 22:21:30 +0900

    something

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,8 @@
-const UL = document.querySelector("ul");
-const FORM_NICKNAME = document.getElementById("nickname");
-const FORM_MESSAGE = document.getElementById("message");
-const NICK_INPUT = FORM_NICKNAME.querySelector("input");
-const FORM_INPUT = FORM_MESSAGE.querySelector("input");
+const UL = document.querySelector('ul');
+const FORM_NICKNAME = document.getElementById('nickname');
+const FORM_MESSAGE = document.getElementById('message');
+const NICK_INPUT = FORM_NICKNAME.querySelector('input');
+const FORM_INPUT = FORM_MESSAGE.querySelector('input');
 
 function makeMessage(type, payload) {
   const msg = { type, payload };
@@ -11,35 +11,41 @@ function makeMessage(type, payload) {
 
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", (msg) => {
+socket.addEventListener('open', (msg) => {
   console.log(msg.type);
 });
 
-socket.addEventListener("close", (msg) => {
+socket.addEventListener('close', (msg) => {
   console.log(msg.type);
 });
 
-socket.addEventListener("error", (msg) => {
+socket.addEventListener('error', (msg) => {
   console.log(msg.type);
 });
 
-socket.addEventListener("message", (handle) => {
-  console.log(handle);
-  const li = document.createElement("li");
-  li.innerText = handle.data;
-  UL.prepend(li);
+socket.addEventListener('message', (handle) => {
+  const msg = JSON.parse(handle.data);
+  console.log(handle.data);
+
+  if (msg.type !== 'nickname') {
+    const li = document.createElement('li');
+    UL.prepend(li);
+    li.innerText = msg.payload;
+  } else {
+    alert(`Nickname changed to ${msg.payload} !!`);
+  }
 });
 
-FORM_MESSAGE.addEventListener("submit", (event) => {
+FORM_MESSAGE.addEventListener('submit', (event) => {
   event.preventDefault();
   const data = FORM_INPUT.value;
-  FORM_INPUT.value = "";
-  socket.send(makeMessage("new_message", data));
+  FORM_INPUT.value = '';
+  socket.send(makeMessage('new_message', data));
 });
 
-FORM_NICKNAME.addEventListener("submit", (event) => {
+FORM_NICKNAME.addEventListener('submit', (event) => {
   event.preventDefault();
   const data = NICK_INPUT.value;
-  NICK_INPUT.value = "";
-  socket.send(makeMessage("nickname", data));
+  NICK_INPUT.value = '';
+  socket.send(makeMessage('nickname', data));
 });
--- src/server.js
+++ src/server.js
@@ -1,43 +1,54 @@
-import express, { json } from "express";
-import WebSocket from "ws";
-import http from "http";
-import { log } from "console";
+import express, { json } from 'express';
+import WebSocket from 'ws';
+import http from 'http';
 
 const app = express();
 
 // set view engine as pus
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 // set public folder, expose for user
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
 // catch all url
-app.get("/*", (_, res) => res.redirect("/"));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const port = 3000;
 
-let sockets = [];
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
-wss.on("connection", (socket) => {
+const sockets = [];
+
+wss.on('connection', (socket) => {
   sockets.push(socket);
-  socket["nickname"] = "Unknown";
-  console.log("connect");
-  socket.on("close", () => {
-    console.log("close");
+  socket['nickname'] = 'Unknown';
+  console.log('connect');
+  socket.on('close', () => {
+    console.log('close');
   });
-  socket.on("message", (handle) => {
+  socket.on('message', (handle) => {
     // console.log(handle);
     const msg = JSON.parse(handle);
     switch (msg.type) {
-      case "new_message":
+      case 'new_message':
         for (const aSocket of sockets) {
-          aSocket.send(`${socket.nickname} : ${msg.payload}`);
+          aSocket.send(
+            makeMessage('new_message', `${socket.nickname} : ${msg.payload}`)
+          );
         }
         break;
-      case "nickname":
-        socket["nickname"] = msg.payload;
+      case 'nickname':
+        if (socket.nickname !== 'Unknown' && socket.nickname !== msg.payload) {
+          socket.send(makeMessage('nickname', msg.payload));
+        }
+        socket['nickname'] = msg.payload;
+        break;
+      default:
         break;
     }
   });
