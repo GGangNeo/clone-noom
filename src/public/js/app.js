6b9cae45d394eeca40b98483612990cb33647528
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-16 22:06:02 +0900

    User Video
    use getMedia, navigator, playinline

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,79 +1,45 @@
 const socket = io();
 
-// const form = document.querySelector('form');
-const welcomeDiv = document.querySelector('#welcome');
-const welcomeForm = welcomeDiv.querySelector('form');
-const roomDiv = document.querySelector('#room');
-const nickForm = document.querySelector(`#nick`);
-const msgForm = document.querySelector('#msg');
+const myFace = document.getElementById('myFace');
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
 
-roomDiv.hidden = true;
-let roomName;
+let muted = false;
+let cameraOff = false;
+let myStream;
 
-function backendDone(msg) {
-  console.log(`The Backend msg: ${msg}`);
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function makeMessage(msg) {
-  const ul = roomDiv.querySelector('ul');
-  const li = document.createElement('li');
-  li.innerText = msg;
-  ul.prepend(li);
-}
+superEventHandler = {
+  handleMuteClick() {
+    if (!muted) {
+      muted = true;
+      muteBtn.innerText = 'Unmute';
+    } else {
+      muted = false;
+      muteBtn.innerText = 'Mute';
+    }
+  },
+  hadleCameraClick() {
+    if (cameraOff) {
+      cameraOff = false;
+      cameraOff.innerText = 'Turn Camera Off';
+    } else {
+      cameraOff = true;
+      cameraOff.innerText = 'Turn Camera On';
+    }
+  },
+};
 
-welcomeForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const input = welcomeForm.querySelector('input');
-  const data = input.value;
-  input.value = '';
-  socket.emit('new_room', data, () => {
-    welcomeDiv.hidden = true;
-    roomDiv.hidden = false;
-    const h3 = roomDiv.querySelector('h3');
-    h3.innerText = `Room ${data}`;
-    roomName = data;
-
-    nickForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const input = nickForm.querySelector('input');
-      const data = input.value;
-      input.value = '';
-      socket.emit('nick', data, roomName, backendDone);
-    });
-
-    msgForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const input = msgForm.querySelector('input');
-      const data = input.value;
-      input.value = '';
-      socket.emit('new_message', data, roomName, backendDone);
-    });
-  });
-});
-
-socket
-  .on('join', (user, newCount) => {
-    const h3 = roomDiv.querySelector('h3');
-    console.log(`new = ${newCount}`);
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    makeMessage(`${user} joined`);
-  })
-  .on('left', (user, newCount) => {
-    const h3 = roomDiv.querySelector('h3');
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    makeMessage(`${user} left`);
-  })
-  .on('new_message', makeMessage)
-  .on('room_change', (rooms) => {
-    const ul = welcomeDiv.querySelector('ul');
-    ul.innerText = '';
-    if (ul.length === 0) return;
-
-    rooms.forEach((e) => {
-      const li = document.createElement('li');
-      const data = e;
-      li.innerText = data;
-      ul.prepend(li);
-    });
-  })
-  .on('nick_change', makeMessage);
+muteBtn.addEventListener('click', superEventHandler.handleMuteClick);
+cameraBtn.addEventListener('click', superEventHandler.hadleCameraClick);
